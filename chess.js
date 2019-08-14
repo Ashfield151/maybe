bcc29aeb011935b5cdcad9f1fde6e065fe1ca2d5
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-14 17:20:06 +0100

    second comment

--- chess.js
+++ chess.js
@@ -1,9 +1,142 @@
-pawn
-knight
-bishop
-rook
-queen
-king
+// pawn
+// knight
+// bishop
+// rook
+// queen
+// king
+
+// board
+// a1
+// a2
+// a3
+// a4
+// a5
+// a6
+// a7
+// a8
+// b1
+// b2
+// b3
+// b4
+// b5
+// b6
+// b7
+// b8 
+// c1
+// c2
+// c3
+// c4
+// c5
+// c6
+// c7
+// c8
+// d1
+// d2
+// d3
+// d4
+// d5
+// d6
+// d7
+// d8
+// e1
+// e2
+// e3
+// e4
+// e5
+// e6
+// e7
+// e8
+// f1
+// f2
+// f3
+// f4
+// f5
+// f6
+// f7
+// f8
+// h1
+// h2
+// h3
+// h4
+// h5
+// h6
+// h7
+// h8
+// g1
+// g2
+// g3
+// g4
+// g5
+// g6
+// g7
+// g8
+
+
+// pieces
+//rules - two pieces on the same square of the same team
+// check rules
+// timer?
+
 
 
 // https://github.com/Ashfield151/maybe.js.git
+
+div
+{
+border:1px solid black;
+width:20px;
+height:20px;
+}
+
+
+<script type="text/javascript">
+
+    // create a chess table 8x8.
+
+    var count = 0;
+
+while (count < 64)
+    {
+
+    if (count % 2 == 0)
+
+        {
+
+        if (count % 8 == 0 && count !=0)
+            {
+            document.write('<br/><div style="background-color:#000000;float:left;">&nbsp</div>');
+
+            }
+        else    
+            {
+            document.write('<div style="background-color:#000000;float:left;">&nbsp</div>');    
+            }
+        }
+
+    else
+
+        {
+        document.write('<div style="background-color:#FFFFFF;float:left;">&nbsp</div>');
+        }
+    /*  
+    */          
+    count++;
+    }
+</script>
+
+
+var table = document.createElement("table");
+for (var i = 1; i < 9; i++) {
+    var tr = document.createElement('tr');
+    for (var j = 1; j < 9; j++) {
+        var td = document.createElement('td');
+        if (i%2 == j%2) {
+            td.className = "white";
+        } else {
+            td.className = "black";
+        }
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+}
+document.body.appendChild(table);
--- day3.js
+++ day3.js
@@ -44,3 +44,113 @@
 // greatGames= favGames.slice(0, 2)
 // console.log(greatGames)
 // console.log
+
+// const pressGrindBeans = () => {
+//     console.log("Grinding for 20 seconds");
+// }
+
+// pressGrindBeans();
+
+// FUNCTION
+// let coffeeIsGrinding = false;
+
+// const pressGrindBeans = () => {
+//     if (coffeeIsGrinding) {
+//         console.log("Stopping the grind");
+//         coffeeIsGrinding = false;
+//     } else {
+//         console.log("Grinding is about to begin");
+//         coffeeIsGrinding = true;
+//     }
+// } 
+// pressGrindBeans();
+
+// const cashWithdrawal = (amount, accnum) => {
+//     console.log(`Withdrawing ${amount} from account ${accnum}`);
+//    }
+   
+//    cashWithdrawal(300, 50449921);
+//    cashWithdrawal(30, 50449921);
+//    cashWithdrawal(200, 50447921);
+
+
+// const coffeeOrder = (size, type) => {
+//     console.log(`You have selected a ${size} ${type}, enjoy`);
+// }
+// coffeeOrder('small', `latte`);
+// coffeeOrder('large', `tea`);
+
+// const takeOrder = (size, drinkType) => {
+//     console.log(`Order received: ${size} ${drinkType}`);
+//    }
+//    takeOrder("Tall","Latte");
+
+// const addUp = (num1, num2) => {
+//     return num1 + num2;
+//    }
+//    addUp(7,3);
+//    console.log(addUp(2,5));
+
+// const multiplyByNineFifths = (celsius) => {
+//     return celsius * (9/5);
+//    };
+//    const getFahrenheit = (celsius) => {
+//     return multiplyByNineFifths(celsius) + 55;
+//    };
+   
+//    console.log("The temperature is " + getFahrenheit(15) + "°F");
+   // Output: The temperature is 59°F
+// const number = 5
+//    function square(number) {
+//     return number * number;
+//    };
+   
+//    square(5);
+// //    // Output: 25
+
+// function factorial (n) {
+//     if ((n === 0) || (n === 1)) {
+//     return 1;
+//     } else {
+//     return (n * factorial(n-1));
+//     }
+//    }
+//    console.log(factorial(4));
+
+// let orderCount = 0;
+// const takeOrder = (topping1, topping2, topping3) => {
+//  console.log(`Pizza with ${topping1}, ${topping2}, ${topping3}`);
+// return (orderCount++);
+// }
+// takeOrder("pineapple", "ham", "sausage");
+// takeOrder("pineapple", "ham", "sausage");
+// takeOrder("pineapple", "ham", "sausage");
+// console.log(orderCount)
+
+
+
+
+
+
+
+ let balance = 100
+ let pin = 1234
+
+
+const cashMachine = (pinN, amount) => {
+     if (pinN == pin && amount <= balance
+        
+        
+        ) {
+        console.log('Take your money');
+    } else if (pinN != pin){
+        console.log(`Please check you have entered your pin corrrectly`);
+    }
+    else{
+
+        console.log("You do not have enough money")}}
+
+
+ cashMachine(1234, 100)
+ 
+
